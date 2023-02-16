f9fe35b800e60dfb8834694e93ba02968f090fb4
Luc Charl Dato <lucharlod@gmail.com>
2023-02-16 17:02:45 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,14 +1,9 @@
 import re
 
-#return ''.join(x + ' ' if x in hahas else x for x in haha)
 
-
-def hahas_separator(strng):
+def haha_separator(strng):
     hahas = ['HA', 'ha', 'Ha', 'hA']
     haha = re.split(r'(HA|ha|Ha|hA)',strng)
-    for x in range(len(haha)):
-        if haha[x] is '':
-            haha[x] = ' '
-    return ''.join(haha)
+    return ''.join(haha[x] if haha[x] != '' else ' ' for x in range(len(haha)) )
      
-print(hahas_separator("Hi, HAHAHAHA! let's go!"))
+print(haha_separator("Hi, HAHAHAHA! let's go!"))
