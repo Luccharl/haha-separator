0a90b2e96e7b72c6d93fe9e1e301236c0dcec85a
Luc Charl Dato <lucharlod@gmail.com>
2023-02-16 17:03:37 +0800

    Update main.py

--- main.py
+++ main.py
@@ -2,7 +2,6 @@ import re
 
 
 def haha_separator(strng):
-    hahas = ['HA', 'ha', 'Ha', 'hA']
     haha = re.split(r'(HA|ha|Ha|hA)',strng)
     return ''.join(haha[x] if haha[x] != '' else ' ' for x in range(len(haha)) )
      
