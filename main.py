00018d5b0cb504d8d3902e923f0969ac765edcf6
Luc Charl Dato <lucharlod@gmail.com>
2023-02-16 20:56:02 +0800

    Update main.py
    updated main.py to keep asking for input even after processing the message.

--- main.py
+++ main.py
@@ -3,6 +3,10 @@ import re
 def haha_separator(strng):
     haha = re.split(r'(HA|ha|Ha|hA)',strng)
     return ''.join(haha[x] if haha[x] != '' else ' ' for x in range(len(haha)))
-     
-strng = input()
-print(haha_separator(strng))
+
+try:
+    while True:     
+        strng = input("Enter your message: ")
+        print(haha_separator(strng))
+except KeyboardInterrupt:
+    exit()
